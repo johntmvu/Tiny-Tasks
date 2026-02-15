866b166957ca9e5b5cc2259c441b4a3707913c87
johntvu <120274177+johnvu124@users.noreply.github.com>
2026-02-14 22:52:51 -0500

    Revert "Merge pull request #4 from johntmvu/jagruthi-sprint1-fixed"
    This reverts commit a98bf74814aa8123d2b2c998164255d0043e64a8, reversing
    changes made to 651cf87d051f50eb10db6a8fac19af4f5251ba0a.

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,13 +1,7 @@
 class Task {
   final String title;
-<<<<<<< HEAD
   final String time;
   bool isDone;
 
   Task({required this.title, required this.time, this.isDone = false});
 }
-=======
-
-  Task({required this.title});
-}
->>>>>>> 134572b (Sprint 1: login screen + basic task add UI)
